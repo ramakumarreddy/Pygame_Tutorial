36631c99976540afbcf5c3372a68d52b52c32bfd
Rama_Kumar_Reddy <ramkrg@outlook.com>
2021-10-15 12:19:15 +0530

    Added enemy to the game window. Controlled player and enemy's movements on game window.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize the pygame.
 pygame.init()
@@ -15,10 +16,22 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("spacecraft.png")
 playerX = 370
 playerY = 480
+playerX_change = 0
+
+# Enemy
+enemyImg = pygame.image.load("alien.png")
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.3
+enemyY_change = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # Game Loop
@@ -31,7 +44,32 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # If keystroke is pressed check whether its right or left.
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+
+        # Setting no movement of warship after key release
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+                playerX_change = 0
+
+    # Warship movement to left or right.
+    playerX += playerX_change
+
+    # Boundaries to player (warship) movement
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    # Boundaries to player (warship) movement
+
+
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
 
 pygame.quit()
